66370a63e674d994bccf97349888e4584afd0d47
kolev77 <kolevi_f@abv.bg>
2016-08-16 16:53:18 +0300

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using MY_DEMO_BLOG.Models;
+
+namespace MY_DEMO_BLOG
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<
+                    ApplicationDbContext,
+                    Migrations.Configuration>()
+                    );
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
