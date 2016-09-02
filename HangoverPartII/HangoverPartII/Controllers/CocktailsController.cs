f59e1cd62ebe8d4820c68ad654b22d6397eb65cd
Silvave <todor.vasilev@yahoo.com>
2016-09-03 00:10:10 +0300

    Adding deleted main project

---
+++ HangoverPartII/HangoverPartII/Controllers/CocktailsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HangoverPartII.Models;
+
+namespace HangoverPartII.Controllers
+{
+    public class CocktailsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Cocktails
+        public ActionResult Index()
+        {
+            //var cocktails = db.Cocktails.Include(c => c.Author).ToList();
+            return View(/*cocktails.ToList()*/);
+        }
+
+        // GET: Cocktails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cocktail cocktail = db.Cocktails.Find(id);
+            if (cocktail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cocktail);
+        }
+
+        // GET: Cocktails/Create
+        public ActionResult Create()
+        {
+            //ViewBag.Author_Id = new SelectList(db.ApplicationUsers, "Id", "FullName");
+            return View();
+        }
+
+        // POST: Cocktails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Image,Title,Body,NetLikeCount")] Cocktail cocktail)
+        {
+            if (ModelState.IsValid)
+            {
+                cocktail.Author = db.Users.FirstOrDefault(u => u.UserName == User.Identity.Name);
+                db.Cocktails.Add(cocktail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            //ViewBag.Author_Id = new SelectList(db.ApplicationUsers, "Id", "FullName");
+            return View(cocktail);
+        }
+
+        // GET: Cocktails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cocktail cocktail = db.Cocktails.Find(id);
+            if (cocktail == null)
+            {
+                return HttpNotFound();
+            }
+            //ViewBag.Author_Id = new SelectList(db.ApplicationUsers, "Id", "FullName");
+            return View(cocktail);
+        }
+
+        // POST: Cocktails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Date,Image,Title,Body,NetLikeCount")] Cocktail cocktail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cocktail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cocktail);
+        }
+
+        // GET: Cocktails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cocktail cocktail = db.Cocktails.Find(id);
+            if (cocktail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cocktail);
+        }
+
+        // POST: Cocktails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cocktail cocktail = db.Cocktails.Find(id);
+            db.Cocktails.Remove(cocktail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HangoverPartII/HangoverPartII/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace HangoverPartII.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Cocktail> Cocktails { get; set; }
+
+        //public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
